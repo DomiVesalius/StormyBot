b08f87618bf5a5dfa52a806cd296207ccc0d1ede
DomiVesalius <mmillion448@gmail.com>
2020-12-27 15:09:07 -0500

    created voice channel 'join' command

--- cogs/music.py
+++ cogs/music.py
@@ -17,6 +17,23 @@ class Music(commands.Cog):
         """
         self.client, self.queue = client, Queue()
 
+    @commands.command(name='join', aliases=['Join'])
+    async def join(self, ctx) -> None:
+        """
+        Makes <self> join the voice channel if any.
+        :param ctx: discord.ext.commands.context.Context
+        """
+        try:  # If the command invoker is in a voice channel
+            channel = ctx.message.author.voice.channel
+            voice = discord.utils.get(self.client.voice_clients, guild=ctx.guild)
+            if voice and voice.is_connected():  # The bot is already connected to channel
+                await ctx.send(f"I am already in {channel}")
+            else:  # The bot is not connected to any voice channel.
+                await channel.connect()
+                await ctx.send(f"Connected to {channel}")
+        except AttributeError:  # Invoker is not in a voice channel.
+            await ctx.send("You need to be in a voice channel for me to join..")
+
 
 def setup(client: discord.ext.commands.bot.Bot) -> None:
     """
