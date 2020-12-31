f5d8d780d043e4dda9696d84f1c072c43558f063
DomiVesalius <mmillion448@gmail.com>
2020-12-31 00:07:37 -0500

    settings file for youtube-dl and ffmpeg

---
+++ lib/ydl_ffmpeg_opts.py
@@ -0,0 +1,12 @@
+YDL_OPTS = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192', }]
+}
+
+FFMPEG_OPTIONS = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn'}
+
