46e942e1f1afc0f8d1740b54953c484a762c8dc2
DomiVesalius <mmillion448@gmail.com>
2021-01-02 21:52:14 -0500

    created a function for profile embeds in social cog

--- embed_maker.py
+++ embed_maker.py
@@ -1,5 +1,6 @@
 from discord import Embed
 from lib.audio import Audio
+from discord.member import Member
 
 
 def enqueue_embed(audio: Audio, requester: str) -> Embed:
@@ -29,4 +30,29 @@ def now_playing_embed(audio: Audio, requester: str) -> Embed:
                           f"``[{audio.get_duration()}]``")
     embed.add_field(name="⠀", value=f"Requested by: {requester}", inline=False)
     embed.set_thumbnail(url=audio.get_thumbnail())
+    return embed
+
+
+def profile_embed(author: Member, member: Member, embed_color: int) -> Embed:
+    """
+    Returns a discord embed containing information about the <member>'s profile.
+    """
+    embed = Embed(color=embed_color)
+    embed.set_author(name=f"{member.name}#{member.discriminator}'s Profile",
+                     icon_url=member.avatar_url)
+    embed.set_footer(text=f"Requested by {author}", icon_url=author.avatar_url)
+
+    roles = ""
+    day, month = member.joined_at.day, member.joined_at.month
+    year = member.joined_at.year
+    embed.add_field(name='Joined at:', value=f"{year}-{month}-{day}")
+
+    for role in member.roles:
+        if role.is_default():  # if the role is @everyone
+            roles += f"{role} "
+        else:
+            roles += f"{role.mention} "
+    embed.add_field(name=f'Roles ({len(member.roles)}):', value=roles, inline=False)
+    embed.add_field(name=f'Top Role:', value=member.top_role.mention, inline=False)
+
     return embed
